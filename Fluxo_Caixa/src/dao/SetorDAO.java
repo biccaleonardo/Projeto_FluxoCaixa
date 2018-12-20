0b200d859c5de88d4b65002c647c388d0c872b88
Antonio Elia Bem David Lombardo <181720103@senacinformatica.br>
2018-12-20 20:46:46 -0200

    setor

--- Fluxo_Caixa/src/dao/SetorDAO.java
+++ Fluxo_Caixa/src/dao/SetorDAO.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dao;
 
-/**
- *
- * @author 181720103
- */
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import model.Setor;
+
+
 public class SetorDAO {
+    
+    public static List<Setor> getSetor(){
+        
+        String sql = "SELECT codigo, nome FROM setor ORDER BY nome ";
+        ResultSet rs = Conexao.consultar(sql);
+        List<Setor> lista = new ArrayList<>();
+        
+        if( rs !=null ){
+            
+            try{
+                while(rs.next()){
+                    Setor set = new Setor();
+                    set.setCodigo(rs.getInt("codigo"));
+                    set.setNome(rs.getString("nome"));
+                    lista.add(set);
+                }
+            }catch (Exception e){
+                JOptionPane.showMessageDialog(null, e.toString());
+            }
+        }
+       return lista; 
+    }
     
 }
