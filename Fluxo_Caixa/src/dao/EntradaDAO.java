828f73689d0b8e10d808a1f5cb0eb1c34d2d9944
Wanderley Mendonca Nobre <181301765@senacinformatica.br>
2019-02-07 20:14:25 -0200

    Config. botão limpar -  entradas e finalização entradaDAO

--- Fluxo_Caixa/src/dao/EntradaDAO.java
+++ Fluxo_Caixa/src/dao/EntradaDAO.java
@@ -5,11 +5,11 @@
  */
 package dao;
 
-
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import javax.swing.JOptionPane;
 import model.Entrada;
 
 /**
@@ -17,80 +17,91 @@ import model.Entrada;
  * @author 181301765
  */
 public class EntradaDAO {
+        public static List<Entrada> getEntrada(){
+        
+        String sql = "SELECT codigo, nome FROM entrada ORDER BY nome ";
+            ResultSet rs = Conexao.consultar(sql);
+            List<Entrada> lista = new ArrayList<>();
+        
+        if( rs !=null ){
+            
+            try{
+                while(rs.next()){
+                    Entrada set = new Entrada();
+                    set.setValor(rs.getInt("valor"));
+                    set.setDescricao(rs.getString("descricao"));
+                    
+                    lista.add(set);
+                }
+            }catch (Exception e){
+                JOptionPane.showMessageDialog(null, e.toString());
+            }
+        }
+       return lista; 
+    }
+    public static Entrada getSaidaByCodigo(int codigo){
+            String sql = "SELECT valor, descricao, data "
+                    + " FROM entrada "
+                    + " WHERE codigo = " + codigo
+                    + " ORDER BY nome "; 
+            
+            
+            ResultSet rs = Conexao.consultar(sql);
+            
+            if(rs != null){
+                
+                try {
+                     
+                    rs.next();
+                                              
+                        Entrada entrada  = new Entrada();
+                        entrada.setValor(rs.getInt( 1 )); 
+                        entrada.setDescricao(rs.getString( 2 )); 
+                        
+                        
+                        
+                        return entrada;
+                        
+                    
+                } catch (Exception e) {
+                }
+             
+                
+            }
+            
+            
+            return null;
+            
+            
+            
+        }
+    
+     public static void inserir(Entrada entrada) {
+         String data =  + entrada.getData().get(Calendar.YEAR)
+                 + "-"  +(entrada.getData().get(Calendar.MONTH )+ 1)
+                 + "-"  + entrada.getData().get(Calendar.DAY_OF_MONTH);
+         
+         
 
-    public static void inserir(Entrada entrada) {
-        String data = "" + entrada.getData().get(Calendar.YEAR)
-                + "-" + (entrada.getData().get(Calendar.MONTH) + 1)
-                + "-" + entrada.getData().get(Calendar.DAY_OF_MONTH);
-
-        String sql = " INSERT INTO entrada"
-                + "( codigo, data, descricao, valor) VALUES ( "
-                + "" + entrada.getCodigo() + " , "
-                + "'" + data + "' , "
-                + "'" + entrada.getDescricao() + "',"
-                + "" + entrada.getValor() + ")";
-
+        
+        String sql = "INSERT  INTO entrada "
+                + "(codigo, data, descricao, valor) VALUES ( "
+                + "  " + entrada.getCodigo()      + ", "
+                + " '" + entrada.getData()        +"',  " 
+                + " '" + entrada.getDescricao()   +"',  " 
+                + "  " + entrada.getValor()       +"  " 
+                + " )";
         Conexao.executar(sql);
     }
 
     public static void editar(Entrada entrada) {
-        String data = "" + entrada.getData().get(Calendar.YEAR)
-                + "-" + (entrada.getData().get(Calendar.MONTH) + 1)
-                + "-" + entrada.getData().get(Calendar.DAY_OF_MONTH);
-
-        String sql = " UPDATE entrada SET"
-                + "( codigo, data, descricao, valor) VALUES ( "
-                + "" + entrada.getCodigo() + " , "
-                + "'" + data + "' , "
-                + "'" + entrada.getDescricao() + "',"
-                + "" + entrada.getValor() + ")";
-
+        String sql = "INSERT  INTO entrada "
+                + "(valor, descricao, data) VALUES ( "
+                + " '" + entrada.getValor()+ "', "
+                + " '" + entrada.getDescricao()+"'  " 
+                + " '" + entrada.getData()+"'  " 
+                + " )";
         Conexao.executar(sql);
     }
-
-    public static void excluir(Entrada entrada) {
-        String sql = " DELETE FROM entrada "
-                + " WHERE codigo = " + entrada.getCodigo();
-        Conexao.executar(sql);
-    }
-
-    public static List<Entrada> getEntradas() {
-        List<Entrada> lista = new ArrayList<>();
-        
-        String sql = "SELECT e.codigo, e.descricao, e.valor, "
-                + " DATE_FORMAT( e.data , '%d' ) , "
-                + " DATE_FORMAT( e.data , '%m' ) , "
-                + " DATE_FORMAT( e.data , '%Y' )  "
-                + " FROM entrada e "
-                + " ORDER BY e.codigo ";
-        
-        ResultSet rs = Conexao.consultar(sql);
-        if (rs !=null){
-            try {
-                while (rs.next()) {
-                    
-                    Entrada entrada = new Entrada();
-                    entrada.setCodigo(rs.getInt(1));
-                    entrada.setDescricao(rs.getString(2));
-                    entrada.setValor(rs.getDouble(3));
-                    
-                    
-                    
-                    //codigo, data, descricao, valor
-                    
-                }
-                
-                
-            } catch (Exception e) {
-            }
-        }
-        
-        
-                
-        
-        
-           
-        
-    }
-
-
+    
+}
